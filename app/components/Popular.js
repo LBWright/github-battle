225742c76676f0e5ee12ece3faf5fb8afd152887
LBWright <lwright.js@gmail.com>
2018-05-22 23:47:21 -0500

    updated lifecycle method and private GridProps component. App renders github to the page

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,36 +1,111 @@
 const React = require('react');
+const PropTypes = require('prop-types');
+const api = require('../utils/api');
+
+function SelectLanguage (props) {
+	let languages = ['All', 'Javascript', 'Python', 'Ruby', 'Java', 'CSS'];		
+	return (
+		<ul className="languages">
+			{languages.map(language => {
+				return (
+					<li 
+					style={
+						language === props.selectedLanguage ?
+						{color : '#d0021b'}:
+						null
+					}
+					onClick={props.onSelect.bind(null, language)}
+					key={language}>
+					{language}
+					</li>
+				) 
+			})}
+		</ul>
+	)
+}
+SelectLanguage.propTypes = {
+	selectedLanguage: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired
+}
+
+function RepoGrid (props) {
+	return (
+	  <ul className='popular-list'>
+		{props.repos.map(function (repo, index) {
+		  return (
+			<li key={repo.name} className='popular-item'>
+			  <div className='popular-rank'>#{index + 1}</div>
+			  <ul className='space-list-items'>
+				<li>
+				  <img
+					className='avatar'
+					src={repo.owner.avatar_url}
+					alt={'Avatar for ' + repo.owner.login}
+				  />
+				</li>
+				<li><a href={repo.html_url}>{repo.name}</a></li>
+				<li>@{repo.owner.login}</li>
+				<li>{repo.stargazers_count} stars</li>
+			  </ul>
+			</li>
+		  )
+		})}
+	  </ul>
+	)
+  }
+
+RepoGrid.propTypes = {
+	repos: PropTypes.array.isRequired
+}
 
 class Popular extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		}
-	}
-	updateLanguage(language){
-		this.setState(function() {
-			return {
-				selectedLanguage: language
-			}
-		})
+
 		this.updateLanguage = this.updateLanguage.bind(this);
+	} //constructor
+
+	componentDidMount() {
+		this.updateLanguage(this.state.selectedLanguage)
+	  }
+
+	  updateLanguage(lang) {
+		this.setState(function () {
+		  return {
+			selectedLanguage: lang,
+			repos: null
+		  }
+	});
+		api.fetchPopularRepos(lang)
+      	.then(function (repos) {
+			this.setState(function () {
+				return {
+					repos: repos
+				}
+			});
+		}.bind(this));
 	}
 	render() {
-		let languages = ['All', 'Javascript', 'Python', 'Ruby', 'Java', 'CSS'];
 
 		return(
-			<ul className="languages">
-				{languages.map(language => {
-					return (
-						<li 
-						key={language}>
-						{language}
-						</li>
-					) 
-				})}
-			</ul>
+				<div>
+					<SelectLanguage 
+						selectedLanguage={this.state.selectedLanguage}
+						onSelect={this.updateLanguage}
+					/>
+					{!this.state.repos ?
+					<p>LOADING...</p> :
+					<RepoGrid repos={this.state.repos}/>
+					}
+					
+				</div>
 		);
 	}
 }
+
 
 module.exports = Popular;
